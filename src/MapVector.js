1a4d02c02a6bb46eeb6020fc1cf52542b0a066a7
Marco Martino <mecmartini@gmail.com>
2019-06-10 18:05:40 +0200

    Init base map with road vector layer

--- src/MapVector.js
+++ src/MapVector.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { createRef, PureComponent } from 'react'
+import { Map, TileLayer, Pane, withLeaflet } from 'react-leaflet'
+import VectorGridDefault from 'react-leaflet-vectorgrid'
+import VectorTileStyling from './VectorTileStyling'
 
-const MapVector = () => {
-  return(
-    <div>Map</div>
-  )
+import 'leaflet/dist/leaflet.css'
+
+// wrap the VectorGrid component using `withLeaflet` HOC
+const VectorGrid = withLeaflet(VectorGridDefault);
+
+const bounds = [
+  [
+    9.40110000000073,
+    -5.51889999982625
+  ],
+  [
+    15.0824999999973,
+    2.40529997196798
+  ]
+];
+
+class MapVector extends PureComponent {
+  _map = createRef();
+
+  render() {
+    const options = {
+    	type: 'protobuf',
+      url: './tiles/{z}/{x}/{y}.pbf',
+      subdomains: 'abcdefg',
+      vectorTileLayerStyles: VectorTileStyling
+    };
+
+    return(
+      <Map className="my-map" ref={this._map} bounds={bounds} zoom={5}>
+        <Pane name="base" style={{ zIndex: 0 }}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </Pane>
+
+        <Pane name="national-roads" style={{ zIndex: 10 }}>
+          <VectorGrid {...options} />
+        </Pane>
+      </Map>
+    )
+  }
 }
 
 export default MapVector
