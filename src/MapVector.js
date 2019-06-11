e86fd39b12c8a1f7d2c8f03e5f064b84174751a4
Marco Martino <mecmartini@gmail.com>
2019-06-11 17:28:55 +0200

    Load layers on click per filter

--- src/MapVector.js
+++ src/MapVector.js
@@ -23,12 +23,35 @@ const bounds = [
 class MapVector extends PureComponent {
   _map = createRef();
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      regionFilter: null,
+      provinceFilter: null,
+      departmentFilter: null,
+    }
+  }
+
   handleClick = (e) => {
-    console.log('click')
-    //console.log(e.target)
+    console.log(e.layer.properties)
+  }
+
+  handleRegionClick = (e) => {
+    this.setState({ regionFilter: e.layer.properties.region })
+  }
+
+  handleProvinceClick = (e) => {
+    this.setState({ provinceFilter: e.layer.properties.province })
+  }
+
+  handleDepartmentClick = (e) => {
+    this.setState({ departmentFilter: e.layer.properties.departement })
   }
 
   render() {
+    const { regionFilter, provinceFilter, departmentFilter } = this.state;
+
     const basemapOptions = {
       type: 'protobuf',
       url: 'https://api.maptiler.com/tiles/v3/{z}/{x}/{y}.pbf?key={key}',
@@ -44,25 +67,84 @@ class MapVector extends PureComponent {
       vectorTileLayerStyles: VectorTileStyling
     };
 
+    const regionStyle = {
+      regions: {
+        fill: true,
+        weight: 0.5,
+        fillColor: '#000000',
+        color: '#000000',
+        fillOpacity: 0.2,
+        opacity: 0.4,
+      }
+    }
+
     const regionsOptions = {
     	type: 'protobuf',
       url: 'http://localhost:8000/public/tiles/regions/{z}/{x}/{y}.pbf',
-      subdomains: 'regions',
-      vectorTileLayerStyles: VectorTileStyling
+      subdomains: regionFilter ? regionFilter : 'regions',
+      vectorTileLayerStyles: regionStyle
     };
+
+    const provinceStyle = {
+      provinces: (properties, zoom) => {
+        const region = properties.region;
+        if (region === regionFilter) {
+          return {
+            fill: true,
+        		weight: 1,
+        		fillColor: '#ff0000',
+        		color: '#ff0000',
+        		fillOpacity: 0.2,
+        		opacity: 0.4
+          }
+        }
+
+        return {
+          fill: false,
+          stroke: false,
+          fillOpacity: 0,
+          opacity: 0,
+          weight: 0,
+        }
+      },
+  	}
 
     const provincesOptions = {
     	type: 'protobuf',
       url: 'http://localhost:8000/public/tiles/provinces/{z}/{x}/{y}.pbf',
-      subdomains: 'provinces',
-      vectorTileLayerStyles: VectorTileStyling
+      subdomains: provinceFilter ? provinceFilter : 'provinces',
+      vectorTileLayerStyles: provinceStyle
     };
+
+    const departementStyle = {
+      departements: (properties, zoom) => {
+        const province = properties.province;
+        if (province === provinceFilter) {
+          return {
+            fill: true,
+        		weight: 1.5,
+        		fillColor: '#00ff00',
+        		color: '#00ff00',
+        		fillOpacity: 0.2,
+        		opacity: 0.4
+          }
+        }
+
+        return {
+          fill: false,
+          stroke: false,
+          fillOpacity: 0,
+          opacity: 0,
+          weight: 0,
+        }
+      },
+  	}
 
     const departementsOptions = {
     	type: 'protobuf',
       url: 'http://localhost:8000/public/tiles/departements/{z}/{x}/{y}.pbf',
-      subdomains: 'departements',
-      vectorTileLayerStyles: VectorTileStyling
+      subdomains: departmentFilter ? departmentFilter : 'departements',
+      vectorTileLayerStyles: departementStyle
     };
 
     const options = {
@@ -91,13 +173,19 @@ class MapVector extends PureComponent {
 
         <Pane name="country" style={{ zIndex: 10 }}>
           <VectorGrid {...countryOptions} onClick={this.handleClick} />
-          <VectorGrid {...regionsOptions} onClick={this.handleClick} />
-          <VectorGrid {...provincesOptions} onClick={this.handleClick} />
-          <VectorGrid {...departementsOptions} onClick={this.handleClick} />
+          <VectorGrid {...regionsOptions} onClick={this.handleRegionClick} />
+
+          { regionFilter &&
+            <VectorGrid key={provinceFilter ? provinceFilter : 'province'} {...provincesOptions} onClick={this.handleProvinceClick} />
+          }
+
+          { provinceFilter &&
+            <VectorGrid key={departmentFilter ? departmentFilter : 'departments'} {...departementsOptions} onClick={this.handleDepartmentClick} />
+          }
         </Pane>
 
         <Pane name="roads" style={{ zIndex: 20 }}>
-          <VectorGrid {...options} />
+          {/* <VectorGrid {...options} /> */}
         </Pane>
       </Map>
     )
